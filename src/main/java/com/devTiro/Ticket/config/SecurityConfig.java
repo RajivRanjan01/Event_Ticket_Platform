6ff68c53511955368923d5e88e4c53e86cb7c963
RajivRanjan01 <rajivranjan12142@gmail.com>
2025-09-19 14:59:11 +0530

    Created the springSecurity configuration file and dissabled the csrf mechanism of our REST API ,COnfigured the springSecurity to be stateless and Configred the spring secutiry to use userProvisioningFilter

---
+++ src/main/java/com/devTiro/Ticket/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.devTiro.Ticket.config;
+
+import com.devTiro.Ticket.filters.UserProvisoningFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            UserProvisoningFilter userProvisioningFilter) throws Exception{
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        //catch all rule
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(
+                                Customizer.withDefaults()
+                        ))
+                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
+        return http.build();
+    }
+}
